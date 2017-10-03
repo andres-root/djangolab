04cef48ad80c18da07b64654879f60f7d518084f
Andres Root <info@andresroot.co>
2017-10-03 14:24:19 -0500

    Django Setup

--- master/settings.py
+++ master/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
